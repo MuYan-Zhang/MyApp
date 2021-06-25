3b4d64816f6cdd1252f9397620945ff459fb1c13
MuYan Zhang <76665125+MuYan-Zhang@users.noreply.github.com>
2021-06-24 19:51:11 -0600

    Multiple windows, simple login

--- main.py
+++ main.py
@@ -9,10 +9,25 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
+from kivy.graphics import Rectangle
+from kivy.graphics import Color
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+class LoginPage(Screen):
+    pass
+
+class SecondPage(Screen):
+    pass
+
+class WindowManager(ScreenManager):
+    pass
+
+kv = Builder.load_file("my.kv")
 
 class MyApp(App):
     def build(self):
-        return FloatLayout()
+        return kv
 
 if __name__ == "__main__":
     MyApp().run()
